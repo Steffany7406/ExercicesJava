2f7f38769237c1c6ce8ce8805a40c329f91356fb
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-11-30 17:18:58 -0300

    Add files via upload

---
+++ Java19/Metodo.java
@@ -0,0 +1,17 @@
+import java.util.Collection;
+import java.util.ArrayList;
+
+
+public class Metodo {
+    public static void main(String[] args) {
+        Collection <String> nomes = new ArrayList<>();
+        nomes.add("Germano");
+        nomes.add("Paulo");
+        nomes.add("Igor");
+        nomes.add("Jefferson");
+        nomes.add("Daiana");
+
+        System.out.println("Lista de Nomes: "+ nomes);
+
+    }
+}
